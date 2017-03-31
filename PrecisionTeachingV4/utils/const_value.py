2d8485e36c4c85eb235ec188fb933b19fdb91418
leiyunhe <yunhe.lei@gmail.com>
2017-03-31 10:08:46 +0800

    init v4

---
+++ PrecisionTeachingV4/utils/const_value.py
@@ -0,0 +1,20 @@
+TIME = '2016-12-20T00:00:00Z'
+REPO_OWNER = 'AIMinder'
+REPO_NAME = 'Py103'
+DATABASE = 'database'
+PAGE = 'index.html'
+
+LABEL = 'task'
+STATE = 'all'
+
+AREA = ['北京','长三角','珠三角','其他']
+
+payload = {'since':TIME}
+payload1 = {'state':STATE,
+			'labels': LABEL,
+			'since':TIME}	 
+
+payload2 = {'labels': LABEL,
+			'state': STATE}
+payload5 = {'state':STATE,
+			'since':TIME}
